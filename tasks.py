460508a78d2abde5bac36f78dfd60d2d126fc354
Patrick Bray <patrickbray17@gmail.com>
2023-10-30 20:38:04 -0400

    working hotkeys script

--- tasks.py
+++ tasks.py
@@ -5,12 +5,6 @@ from invoke import task
 from loguru import logger
 
 
-def safe_update(new, old):
-    for key, val in new.items():
-        if key in old.keys() and val is not old[key]:
-            logger.warning(f"New {key} value:\n{val}\nOld {key} value {old[key]}")
-
-
 @task
 def sync_hotkeys(c, parent_dir: str = "."):
     """
@@ -18,16 +12,49 @@ def sync_hotkeys(c, parent_dir: str = "."):
     It then combines every hotkeys file into one hotkey file and writes that file over all the existing hotkey files.
     """
 
-    combined_hotkeys = {}
 
+import json
+import os
+
+from invoke import task
+from loguru import logger
+
+
+@task
+def sync_hotkeys(c, parent_dir: str = "."):
+    """
+    This script crawls through a directory and finds every obsidian hotkeys.json file.
+    It then combines every hotkeys file into one hotkey file and overwrites all the existing hotkey files
+    starting with the least recently updated ones.
+    """
+
+    combined_hotkeys = {}
+    file_modification_times = {}
+
+    # Collect modification times of 'hotkeys.json' files
     for dirpath, dirnames, filenames in os.walk(parent_dir):
         for filename in filenames:
             if filename == "hotkeys.json":
                 filepath = os.path.join(dirpath, filename)
-                with open(filepath, "r") as f:
-                    data = json.load(f)
-                combined_hotkeys.update(data)
-    logger.info("\n" + json.dumps(combined_hotkeys, indent=2))
+                file_modification_times[filepath] = os.stat(filepath).st_mtime
+
+    # Sort files by modification time, in ascending order (oldest first)
+    sorted_files = sorted(file_modification_times.items(), key=lambda x: x[1])
+
+    for filepath, _ in sorted_files:
+        logger.info(filepath)
+        with open(filepath, "r") as f:
+            data = json.load(f)
+        combined_hotkeys.update(data)
+
+    # Write the combined hotkeys back to all files
+    for filepath, _ in sorted_files:
+        with open(filepath, "w") as f:
+            json.dump(combined_hotkeys, f, indent=2)
+
+    logger.info(
+        "Combined hotkeys have been written to all 'hotkeys.json' files, starting with the least recently updated ones."
+    )
 
 
 @task
